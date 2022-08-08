c376c2c040e1128c7cd992e12e6518669c9ff679
Ella <lunar.politics@hmail.com>
2022-08-08 23:32:12 +0300

    faq section added

--- public/js/common.js
+++ public/js/common.js
@@ -383,8 +383,29 @@ function eventHandler() {
 	});
 
 	const swiperSert = new Swiper('.slider-sert--js', { 
-		slidesPerView: 3,
-		spaceBetween: 33,
+		// slidesPerView: 4,
+		// spaceBetween: 33,
+		breakpoints: {
+			// when window width is >= 320px
+			320: {
+				slidesPerView: 1,
+				spaceBetween: 20
+			},
+			420: {
+				slidesPerView: 2,
+				spaceBetween: 25
+			},
+			// when window width is >= 480px
+			768: {
+				slidesPerView: 3,
+				spaceBetween: 30
+			},
+			// when window width is >= 640px
+			1024: {
+				slidesPerView: 4,
+				spaceBetween: 33
+			}
+		},
 		navigation: {
 			nextEl: '.swiper-button-next',
 			prevEl: '.swiper-button-prev',
